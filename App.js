7460ab4ec7e12cb5555540fe4110fa2edd66a12e
SanthoshBhandari <ssantu28964@gmail.com>
2025-11-18 11:32:19 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, TextInput } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  const [calcData, setCalcData] = useState({num:20.0, result:null});
+  const doCalc = () => {
+    if(isNaN(calcData.num)){
+      setCalcData({...calcData, result : 'Invalid Number'})
+      return;
+    }
+
+    const num = calcData.num;
+    const sqr = num*num;
+    setCalcData({...calcData, result:`Result:${sqr}`})
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>This is Square Calculator App</Text>
+      <TextInput value ={calcData.num} keyboardType='numeric'
+        placeholder='Enter Number'
+        onChangeText={(data)=>setCalcData({...calcData, num: data})}/>
+      <Button title='Find Square' onPress={doCalc}/>
+      <Text>{calcData.result}</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(200, 219, 255, 1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
